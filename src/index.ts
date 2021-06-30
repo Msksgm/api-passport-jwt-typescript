a0da0771161cb7f9f121aae9339c2e2b0a3b6cfb
Msksgm <msksgm3124@gmail.com>
2021-06-30 21:58:16 +0900

    add: passport.jsと/catと/userを追加

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 
+import passport from "./lib/security";
+
+import catRouter from "./route/cat";
+import userRouter from "./route/user";
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(passport.initialize());
+app.use("/cat", catRouter);
+app.use("/user", userRouter);
 
 app.listen(3000, () => {
   console.log("listen to " + 3000);
